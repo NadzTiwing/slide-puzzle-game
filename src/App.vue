6559caaa3169fc0a123f50c3af3ab33ea77517f5
Nadia Tiwing <tiwingnadia@gmail.com>
2024-03-14 23:12:09 +0800

    update game

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
-<script>
+<script setup lang="ts">
+import Puzzle from './components/Puzzle.vue';
+import { ref } from "vue";
 
-
-export default {
-  name: 'App',
-  
-}
+const isVisible = ref(true);
+const isStart = ref(false);
+const start = () => {
+  isVisible.value = false;
+  isStart.value = true;
+}; 
 </script>
 
 <template>
-  <router-view></router-view>    
+  <button class="start-btn" @click="start" v-show="isVisible">Start Game</button>
+  <main v-show="isStart">
+    <Puzzle />
+  </main>
 </template>
 
+<style scoped>
+.start-btn {
+  display: flex;
+  place-content: center;
+  padding: 1rem 2rem;
+  background-color: white;
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+  margin: 40vh auto;
+  border: 1px solid lightgreen;
+  border-radius: 50px;
+}
+
+.start-btn:hover {
+  background-color: lightgreen;
+  color: white;
+}
+</style>
---
+++ src/components/Puzzle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+      <span id="moves">0</span>
+      <div class="slider-game">
+        <div class="container">
+        </div>
+        <div class="original-image">
+          <img src="https://groundtutorial.com/wp-content/uploads/2024/01/original_image.png" alt="puzzle-picture" />
+        </div>
+      </div>
+    </main>
+</template>
